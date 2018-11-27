2d1c3ab9e1d134c96abd35c3b4fcecb98626ea3e
fbeltrao <fbeltrao@windowslive.com>
2018-11-27 08:54:07 +0100

    Add integration tests

--- test/CmdTest/CmdTest.cs
+++ test/CmdTest/CmdTest.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace CmdTest
 {
-    public class UnitTest1
+    public class CmdTest
     {
         [Fact]
-        public void Test1()
+        public void Should_Be_True_In_Unit_Test()
         {
             Assert.True(true);
         }
---
+++ test/integration/CmdIntegrationTest/CmdIntegrationTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace CmdIntegrationTest
+{
+    public class CmdIntegrationTest
+    {
+        [Fact]
+        public void Should_Be_True_In_Integration_Test()
+        {
+            Assert.True(true);
+
+        }
+    }
+}
